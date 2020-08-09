632fa4bb49a01332fff0d225517339a7d09bda1f
Crystal Prusek <crystal.prusek@gmail.com>
2020-08-09 21:05:06 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,30 +1,4 @@
-require_relative "../lib/contacts.rb"
 
-puts remove_strawberry
-puts remove_strawberry(contacts = {
-    "Jon Snow" => {
-      name: "Jon",
-      email: "jon_snow@thewall.we", 
-      favorite_icecream_flavors: ["chocolate", "vanilla"]
-    },
-    "Freddy Mercury" => {
-      name: "Freddy",
-      email: "freddy@mercury.com",
-      favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
-    }
-  })
-
-def remove_strawberry(contacts)
-  contacts.each do |person, contact_details_hash|
-    contact_details_hash.each do |attribute, data|
-      if attribute == :favorite_icecream_flavors
-        data.delete_if { |ice_cream| ice_cream == "strawberry"}
-      end
-    end
-  end
-end
-
-# Alternative solution
 
 def remove_strawberry(contacts)
   # Assign Freddy Mercury's hash to a variable
